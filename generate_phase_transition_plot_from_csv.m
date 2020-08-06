4a9dd8b362afe693ad180855e5ab465553b29f58
Nick Dexter <dexter.nicholas@gmail.com>
2020-08-05 21:40:33 -0700

    Adding plot

--- generate_phase_transition_plot_from_csv.m
+++ generate_phase_transition_plot_from_csv.m
@@ -5,11 +5,11 @@ set(gcf,'Color',[1 1 1]);
 
 %Position plot at left hand corner with width 10 and height 10.
 % set(gcf, 'PaperPosition', [0 0 20 10]); 
-set(gcf, 'PaperPosition', [0 0 10 10]); 
+set(gcf, 'PaperPosition', [0 0 20 10]); 
 %Set the paper to have width 10 and height 10.
 % set(gcf, 'PaperSize', [20 10]);
-set(gcf, 'PaperSize', [10 10]);
-set(gcf,'Position',[200 200 800 800])
+set(gcf, 'PaperSize', [20 10]);
+set(gcf,'Position',[200 200 1600 1600])
 
 % open ~/Downloads/CM.csv
 % CM = CM{:,:};

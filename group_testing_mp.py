0d9dd6e87f5d276e47e7c493624d5c32d38d2346
Hooman Zabeti <hoomanzabeti@gmail.com>
2020-12-13 20:19:20 -0800

    config file added

--- group_testing_mp.py
+++ group_testing_mp.py
@@ -3,6 +3,7 @@
 """
 @author: hzabeti
 """
+from typing import Dict, List, Any, Union, Hashable
 
 from generate_groups import gen_measurement_matrix
 from generate_individual_status import gen_status_vector
@@ -21,20 +22,35 @@ import os
 import uuid
 import datetime
 import time
+import yaml
 
 
-def result_path_generator(noiseless, LP_relaxation, N, group_size, name):
+# def result_path_generator(noiseless, LP_relaxation, N, group_size, name):
+#     currentDate = datetime.datetime.now()
+#     dir_name = currentDate.strftime("%d") + '_' + currentDate.strftime("%b") + '_' + currentDate.strftime("%Y")
+#     if noiseless:
+#         noiseless = 'NoiseLess'
+#     else:
+#         noiseless = 'Noisy'
+#     if LP_relaxation:
+#         LP_relaxation = 'LP'
+#     else:
+#         LP_relaxation = 'ILP'
+#     local_path = "./Results/{}/{}/{}".format(dir_name, noiseless, LP_relaxation)
+#     path = os.getcwd()
+#     if not os.path.isdir(local_path):
+#         try:
+#             os.makedirs(path + local_path[1:])
+#         except OSError:
+#             print("Creation of the directory %s failed" % path + local_path[1:])
+#         else:
+#             print("Successfully created the directory %s " % path + local_path[1:])
+#     return path + local_path[1:] + "/{}_{}_{}_{}.csv".format(name, N, group_size, currentDate.strftime("%H:%M:%S"))
+def path_generator(file_path, file_name, file_format):
     currentDate = datetime.datetime.now()
-    dir_name = currentDate.strftime("%d") + '_' + currentDate.strftime("%b") + '_' + currentDate.strftime("%Y")
-    if noiseless:
-        noiseless = 'NoiseLess'
-    else:
-        noiseless = 'Noisy'
-    if LP_relaxation:
-        LP_relaxation = 'LP'
-    else:
-        LP_relaxation = 'ILP'
-    local_path = "./Results/{}/{}/{}".format(dir_name, noiseless, LP_relaxation)
+    # TODO: change dir_name to unique code if you want to run multiple configs
+    dir_name = currentDate.strftime("%b_%d_%Y_%H_%M")
+    local_path = "./Results/{}".format(dir_name)
     path = os.getcwd()
     if not os.path.isdir(local_path):
         try:
@@ -43,12 +59,9 @@ def result_path_generator(noiseless, LP_relaxation, N, group_size, name):
             print("Creation of the directory %s failed" % path + local_path[1:])
         else:
             print("Successfully created the directory %s " % path + local_path[1:])
-    return path + local_path[1:] + "/{}_{}_{}_{}.csv".format(name, N, group_size, currentDate.strftime("%H:%M:%S"))
-
+    return path + local_path[1:] + "/{}.{}".format(file_name, file_format)
 
 def multi_process_group_testing(opts, param):
-    current_directory = os.getcwd()
-    temp_unique_key = uuid.uuid1()
     for method in opts['test_noise_methods']:
         print('adding ' + method + ' noise', end=' ')
         if method == 'truncation':
@@ -65,21 +78,9 @@ def multi_process_group_testing(opts, param):
             print('with permutation_noise_probability = ' + str(opts['permutation_noise_prob']))
     # specify the file name for generating MATLAB save files
     opts['data_filename'] = opts['run_ID'] + '_generate_groups_output.mat'
-    # file_path = os.path.join(current_directory, r'{}_problem.mps'.format(temp_unique_key))
-    # param['file_path'] = file_path
 
-    currentDate = datetime.datetime.now()
-    dir_name = currentDate.strftime("%d") + '_' + currentDate.strftime("%b") + '_' + currentDate.strftime("%Y")
-    local_path = "./Logs/{}".format(dir_name)
-    path = os.getcwd()
-    if not os.path.isdir(local_path):
-        try:
-            os.makedirs(path + local_path[1:])
-        except OSError:
-            print("Creation of the directory %s failed" % path + local_path[1:])
-        else:
-            print("Successfully created the directory %s " % path + local_path[1:])
     try:
+        single_run_start = time.time()
         # generate the measurement matrix from the given options
         A = gen_measurement_matrix(opts)
         # generate the infected status of the individuals
@@ -88,18 +89,22 @@ def multi_process_group_testing(opts, param):
 
         # generate the data corresponding to the group tests
         b = gen_test_vector(A, u, opts)
-        param['solver_options']['logPath'] = path + local_path[1:] + '/log_{}_{}_{}_{}.txt'.format(opts['N'],
+        param['solver_options']['logPath'] = log_path+ '/log_{}_{}_{}_{}_{}.txt'.format(opts['N'],
                                                                                                    opts['group_size'],
-                                                                                                   opts['m'], opts['s'])
-        print(param['solver_options']['logPath'])
+                                                                                                   opts['m'], opts['s'],opts['seed'])
         c = GroupTestingDecoder(**param)
+        single_fit_start = time.time()
         c.fit(A, b)
+        single_fit_end = time.time()
         print('SUM', np.sum(A, axis=0))
         print('Score:', c.score(A, b))
         # evaluate the accuracy of the solution
         ev_result = decoder_evaluation(u, c.solution())
+        single_run_end= time.time()
         # TODO: this is only for cplex status! Change it to more general form!
         ev_result['Status'] = c.prob_.cplex_status
+        ev_result['solver_time'] = round(single_fit_end-single_fit_start,2)
+        ev_result['time'] = round(single_run_end - single_run_start,2)
 
         # evaluate the accuracy of the solution
         # ev_result = decoder_evaluation(u, sln, opts['N'])
@@ -118,53 +123,89 @@ def multi_process_group_testing(opts, param):
     return ev_result
 
 
+def config_decoder(config_inpt):
+    '''
+    TODO: I used the following stackoverflow post for the return part:
+    https://stackoverflow.com/questions/5228158/cartesian-product-of-a-dictionary-of-lists
+    '''
+    for keys, vals in config_inpt.items():
+        if isinstance(vals, dict):
+            print(vals)
+            if config_inpt[keys]['mode'] == 'continuous':
+                config_inpt[keys] = np.arange(*config_inpt[keys]['values'])
+            elif config_inpt[keys]['mode'] == 'discrete':
+                config_inpt[keys] = config_inpt[keys]['values']
+            elif config_inpt[keys]['mode'] == 'exact':
+                config_inpt[keys].pop('mode')
+                config_inpt[keys] = [config_inpt[keys]]
+        else:
+            config_inpt[keys] = [vals]
+    return [dict(zip(config_inpt.keys(), vals)) for vals in itertools.product(*config_inpt.values())]
+
+
 # main method for testing
 if __name__ == '__main__':
-    # options for setting up group testing problem
     start_time = time.time()
-    seed_list = range(10)
-    # rho_list = [0.1]
-    N_list = [1000]
-    # prevalence_list = np.concatenate((np.arange(0.005,0.01,0.005),np.arange(0.01, 0.11, 0.01)))
-    prevalence_list = [0.05, 0.1]
-    group_size_list = [16]
-    m_list = np.arange(0.01, 1.01, 0.01)
-    divisibility_list = [16]
+    # Read config file
+    with open("config.yml", 'r') as config_file:
+        config_dict = yaml.load(config_file, Loader=yaml.FullLoader)
+    # Load opts and param
+    opts = config_decoder(config_dict['opts'])
+    param = config_decoder(config_dict['param'])
+    # output files path
+    path = os.getcwd()
+    currentDate = datetime.datetime.now()
+    dir_name = currentDate.strftime("%b_%d_%Y_%H_%M")
+    result_path = os.path.join(os.getcwd(), "Results/{}".format(dir_name))
+    log_path = os.path.join(result_path, "Logs")
+    if not os.path.isdir(log_path):
+        try:
+            os.makedirs(log_path)
+        except OSError:
+            print("Creation of the directory %s failed" % log_path)
+        else:
+            print("Successfully created the directory %s " % log_path)
 
-    # opts = [{'run_ID': 'debugging', 'verbose': False, 'plotting': False, 'saving': True, 'm': int((p + round(1 / g,
-    # 3)) * N), 'N': N, 's': int((p + round(1 / g, 3)) * N * r), 'seed': seed, 'group_size': g,
-    # 'max_tests_per_individual': d, 'graph_gen_method': 'no_multiple', 'test_noise_methods': ['truncation'],
-    # 'delta': round(p + round(1 / g, 3), 3), 'rho': round(r, 3)} for seed in seed_list for N in N_list for g in
-    # group_size_list for p in m_list for r in rho_list for d in divisibility_list]
+    # # options for setting up group testing problem
+    # seed_list = range(10)
+    # # rho_list = [0.1]
+    # N_list = [1000]
+    # # prevalence_list = np.concatenate((np.arange(0.005,0.01,0.005),np.arange(0.01, 0.11, 0.01)))
+    # prevalence_list = [0.05, 0.1]
+    # group_size_list = [16]
+    # m_list = np.arange(0.01, 1.01, 0.01)
+    # divisibility_list = [16]
+    #
+    # # opts = [{'run_ID': 'debugging', 'verbose': False, 'plotting': False, 'saving': True, 'm': int((p + round(1 / g,
+    # # 3)) * N), 'N': N, 's': int((p + round(1 / g, 3)) * N * r), 'seed': seed, 'group_size': g,
+    # # 'max_tests_per_individual': d, 'graph_gen_method': 'no_multiple', 'test_noise_methods': ['truncation'],
+    # # 'delta': round(p + round(1 / g, 3), 3), 'rho': round(r, 3)} for seed in seed_list for N in N_list for g in
+    # # group_size_list for p in m_list for r in rho_list for d in divisibility_list]
+    #
+    # opts = [{'run_ID': 'debugging', 'verbose': False, 'plotting': False, 'saving': True,
+    #          'm': int(m * N), 'N': N, 's': int(N * p),
+    #          'seed': seed, 'group_size': g, 'max_tests_per_individual': d, 'graph_gen_method': 'no_multiple',
+    #          'test_noise_methods': ['truncation']} for seed
+    #         in seed_list for N in N_list for g in
+    #         group_size_list
+    #         for m in m_list for d in divisibility_list for p in prevalence_list]
 
-    opts = [{'run_ID': 'debugging', 'verbose': False, 'plotting': False, 'saving': True,
-             'm': int(m * N), 'N': N, 's': int(N * p),
-             'seed': seed, 'group_size': g, 'max_tests_per_individual': d, 'graph_gen_method': 'no_multiple',
-             'test_noise_methods': ['truncation']} for seed
-            in seed_list for N in N_list for g in
-            group_size_list
-            for m in m_list for d in divisibility_list for p in prevalence_list]
-
-    param = {'lambda_w': 1, 'lambda_p': 100, 'lambda_n': 100, 'fixed_defective_num': None,
-             'sensitivity_threshold': None,
-             'specificity_threshold': None, 'is_it_noiseless': True, 'lp_relaxation': False, 'solver_name': 'CPLEX_PY',
-             'solver_options': {'timeLimit': 600}}
+    # param = {'lambda_w': 1, 'lambda_p': 100, 'lambda_n': 100, 'fixed_defective_num': None,
+    #          'sensitivity_threshold': None,
+    #          'specificity_threshold': None, 'is_it_noiseless': True, 'lp_relaxation': False, 'solver_name': 'CPLEX_PY',
+    #          'solver_options': {'timeLimit': 600}}
 
     with Pool(cpu_count()) as pool:
-        results = pool.starmap(multi_process_group_testing, itertools.product(opts, [param]))
+        results = pool.starmap(multi_process_group_testing, itertools.product(opts, param))
         pool.close()
         pool.join()
-    # column_names = ['N', 'm', 's', 'group_size', 'seed', 'delta', 'rho', 'tn', 'fp', 'fn', 'tp']
-    column_names = ['N', 'm', 's', 'group_size', 'seed', 'max_tests_per_individual', 'tn', 'fp', 'fn', 'tp',
-                    'balanced_accuracy', 'Status']
-    # Saving files
-    opts_path = result_path_generator(param['is_it_noiseless'], param['lp_relaxation'], N_list[0], group_size_list[0],
-                                      'opts')
-    pd.DataFrame(opts).to_csv(opts_path)
 
-    result_path = result_path_generator(param['is_it_noiseless'], param['lp_relaxation'], N_list[0], group_size_list[0],
-                                        'CM')
-    pd.DataFrame(results).reindex(columns=column_names).to_csv(result_path)
+    column_names = ['N', 'm', 's', 'group_size', 'seed', 'max_tests_per_individual', 'tn', 'fp', 'fn', 'tp',
+                    'balanced_accuracy', 'Status','solver_time','time']
+    # Saving files
+    pd.DataFrame(opts).to_csv(os.path.join(result_path,'opts.csv'))
+    pd.DataFrame(results).reindex(columns=column_names).to_csv(os.path.join(result_path,'CM.csv'))
+
     end_time = time.time()
     print(end_time - start_time)
     # final report generation, cleanup, etc.

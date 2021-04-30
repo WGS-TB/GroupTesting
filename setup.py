2cd53c1c48ef0fa51ed6f4ea87545a4ed4afcce0
Joel Simoneau <joel.simoneau@usherbrooke.ca>
2021-04-30 13:21:51 -0400

    update setup

--- setup.py
+++ setup.py
@@ -6,12 +6,13 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name=_program,
+    name="GroupTesting",
     version=__version__,
     author="Hooman Zabeti, Nick Dexter",
+    author_email="hzabeti@sfu.ca, nicholas_dexter@sfu.ca"
     description="Group testing for SARS-CoV-2 in large populations.",
     url="https://github.com/WGS-TB/GroupTesting",
-    license="TBD",
+    license="GPL-3",
     packages=["group_testing"],
     entry_points="""
     [console_scripts]
